696e5c754b207c4af2adbab718f305f9a70df7e1
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2023-01-25 22:52:28 +0530

    Update data.tf

--- modules/auto-scalling/data.tf
+++ modules/auto-scalling/data.tf
@@ -1,3 +1,3 @@
 data "template_file" "user_data" {
-    template = file("/Users/dhruvins/Desktop/AWS_Autoscalling_LoadBalancer_Terraform/modules/auto-scalling/container.sh")
+    template = file("container.sh")
 }
